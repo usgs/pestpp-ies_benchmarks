5baa3bc21ede3d7e358beda7fd2dcf1c751758c4
unknown <jtwhite1000@gmail.com>
2020-08-31 16:25:48 -0600

    more on pcs summary, added testing

--- ies_test_part2.py
+++ ies_test_part2.py
@@ -76,7 +76,7 @@ noptmax = 3
 
 compare_files = ["pest.phi.actual.csv", "pest.phi.meas.csv", "pest.phi.regul.csv",
                  "pest.{0}.par.csv".format(noptmax), "pest.{0}.obs.csv".format(noptmax),
-                 "pest.{0}.par.csv".format(0), "pest.obs+noise.obs.csv"]
+                 "pest.{0}.par.csv".format(0), "pest.obs+noise.csv"]
 diff_tol = 1.0e-6
 port = 4016
 num_reals = 10
